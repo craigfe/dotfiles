e6d26f8ad104b9f85248a816e934e55731fa29ee
Craig Ferguson <craig.ferguson@sumdog.com>
2018-08-17 10:40:57 +0100

    Add colour calculation to kelvin_colour script

--- scripts/kelvin_colour.py
+++ scripts/kelvin_colour.py
@@ -5,11 +5,39 @@
 # http://www.zombieprototypes.com/?p=210
 
 from __future__ import print_function
+from math import pow, log
 import sys
 
 def eprint(*args, **kwargs):
     """Print a series of strings to stderr."""
     print(*args, file=sys.stderr, **kwargs)
+
+def clamp(n):
+    """Take a floating point value and return the nearest integer bounded within [0, 255]"""
+    return max(min(int(round(n)), 255), 0)
+
+def rgb_to_hex(rgb):
+    return '#%02x%02x%02x' % rgb
+
+def calculate_red(temperature):
+    if temperature < 6600:
+        return 255
+
+    return 329.698727446 * pow(temperature / 100 - 60, -0.1332047592)
+
+def calculate_green(temperature):
+    if temperature <= 6600:
+        return 99.4708025861 * log(temperature / 100) - 161.1195681661
+
+    return 288.1221695283 * pow(temperature / 100 - 60, -0.0755148492)
+
+def calculate_blue(temperature):
+    if temperature >= 6600:
+        return 255
+    if temperature <= 1900:
+        return 0
+
+    return 138.5177312231 * log(temperature / 100 - 10) - 305.0447927307
 
 def main(argv):
     """Convert a Kelvin temperature to an RGB string."""
@@ -17,9 +45,16 @@ def main(argv):
         eprint("Usage: kelvin_colour <temperature>")
         return 1
 
-    print(argv[0])
-    return 0
+    if not argv[1].isdigit():
+        eprint("Temperature must be a positive integer")
+        return 2
 
+    temperature = int(argv[1])
+    floatColours = [f(temperature) for f in [calculate_red, calculate_blue, calculate_green]]
+    intColours = tuple([clamp(colour) for colour in floatColours])
+
+    print(rgb_to_hex(intColours))
+    return 0
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
--- scripts/test_kelvin_colour.py
+++ scripts/test_kelvin_colour.py
@@ -1,9 +1,18 @@
 import pytest
 import unittest
-
-import kelvin_colour
+import kelvin_colour as kc
 
 class ScriptTests(unittest.TestCase):
-    def test_main(self):
-        with self.assertRaises(SystemExit):
-            kelvin_colour.main(['test_kelvin_colour.py']) # Empty
+
+    def test_emptyParams(self):
+       assert (kc.main(['test_kelvin_colour.py']) == 1)
+
+    def test_nonInteger(self):
+        assert (kc.main(['test_kelvin_colour.py', '0K']) == 2)
+
+    def test_clamp(self):
+        assert kc.clamp(0) == 0
+        assert kc.clamp(255) == 255
+        assert kc.clamp(123) == 123
+        assert kc.clamp(3.14159265) == 3
+        assert kc.clamp(254.5001) == 255
