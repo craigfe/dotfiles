dbcde98bff2a64248b28996d1a94237aa03ca4c7
Craig Ferguson <cf443@cam.ac.uk>
2018-08-25 12:39:45 +0100

    Remove i3 and vis theme colours from source control

--- i3/config.d/colours.sh
+++ i3/config.d/colours.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-accent='#FFAF1B'
+accent=$(cat ~/repos/dotfiles/colours/out/theme)
+
+# Default to white if theme has not yet been set
+[[ -z $accent ]] && accent="#ffffff"
 
 cat << EOF
 # class                 border  backgr. text    indicator child_border
 client.focused         $accent $accent $accent $accent
-
 # client.focused	#0099ff #0099ff #000000 #0099ff
 # client.unfocused	#666666 #666666b #000000 #66666
 # client.urgent	#ff0000 #ff0000 #000000 #ff0000
