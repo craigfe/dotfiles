e88e63abe08747fac37fa33a1e1f9d11f58e254d
Craig Ferguson <me@craigfe.io>
2021-10-25 22:35:06 +0100

    nix: fix on MacOS

--- nix/user.nix
+++ nix/user.nix
@@ -1,12 +1,14 @@
-with import <nixpkgs> { }; [
+with import <nixpkgs> { };
+[
+  (hiPrio clang)
   (hiPrio gcc_multi)
   bat
   binutils-unwrapped
-  bubblewrap
-  clang
+  cacert # required on MacOS (otherwise NIX_SSL_CERT_FILE will be unset)
   cloc
   curl
   diff-so-fancy
+  diffutils # built-in `diff` on MacOS is archaic
   docker
   docker-compose
   fzf
@@ -15,25 +17,32 @@ with import <nixpkgs> { }; [
   git-lfs
   github-cli
   gnumake
+  grep
   htop
+  hub
   jq
   lnav
   mons
   moreutils # provides `vipe`, `vidir` etc.
+  ncdu
   neovim
   nix
   nixfmt
-  polybar
   ranger
   ripgrep
   rsync
   rustup
   stack
   stow
-  strace
   tectonic
   tmux
   unclutter
   wget
+  yarn
   zsh
-]
+] ++ (if system != "aarch64-darwin" then [
+  bubblewrap # used by `opam`
+  strace
+  polybar
+] else
+  [ fswatch ])
